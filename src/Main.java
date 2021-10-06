c1de11f12a329224039517e9b2f9e914025d1276
Sim0n <57576661+simon-stentoft@users.noreply.github.com>
2021-10-06 23:16:03 +0200

    wqeqwe

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,10 @@ public class Main {
         System.out.println(person1);
 
         Person person2 = new Person(2,"Ken","Adelvej 66","Nat patrulje",LocalDate.of(2008,6,4),true);
+        System.out.println(person2);
+
+        Tilmeldingskatalog tilmeldingskatalog1 = new Tilmeldingskatalog("Sommer lejr",false,LocalDate.of(2020,5,16),LocalDate.of(2020,5,20));
+        System.out.println(tilmeldingskatalog1);
+
     }
 }
